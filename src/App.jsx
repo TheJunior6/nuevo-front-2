746c79b2ac7125fd70c897e4f2dc1e4870412c3c
Ruben <sanchezruben696@gmail.com>
2024-11-08 16:12:22 -0500

    copia

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,29 @@
 import './App.css';
 import Form from './components/Form';
 import UserHome from './components/UserHome';
-import Ganadores from './components/Ganadores';
+import Ganadores from './components/ganadores';
 import AdminHome from './components/AdminHome';
-import ChangePassword from './components/ChangePassword';
-import Crearusuarios from './components/CrearUsuarios';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
-import { useState, useEffect } from 'react';
-
-// Componente para rutas protegidas
-function ProtectedRoute({ element, user }) {
-  if (!user) {
-    return <Navigate to="/" replace />; // Redirige al login si no está autenticado
-  }
-  return element;
-}
+import ChangePassword from './components/cambiarP';
+import Crearusuarios from './components/crearusers';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import { useState } from 'react';
 
 function App() {
   const [user, setUser] = useState(null);
-
-  // Simulación de carga de usuario (puedes hacer una llamada API aquí)
-  useEffect(() => {
-    // Aquí podrías obtener los datos del usuario de un API o localStorage
-    // Si el usuario está logueado, puedes setearlo en el estado
-    // Ejemplo:
-    // const loggedUser = localStorage.getItem('user');
-    // if (loggedUser) setUser(loggedUser);
-  }, []);
-
   return (  
     <BrowserRouter>
+      {/* <Navigation/> */}
       <Routes>
-        <Route index element={<Form callback={setUser} />} />
-        <Route path="/cambiarP" element={<ChangePassword />} />
-        <Route path="/crearusers" element={<Crearusuarios />} />
-        <Route path="/ganadores" element={<Ganadores />} />
-        
-        {/* Rutas protegidas */}
-        <Route 
-          path="/UserHome" 
-          element={<ProtectedRoute user={user} element={<UserHome user={user} />} />} 
-        />
-        <Route 
-          path="/adminHome" 
-          element={<ProtectedRoute user={user} element={<AdminHome user={user} />} />} 
-        />
+        <Route index element={<Form callback={setUser}/>}></Route>
+        <Route path='/cambiarP' element={<ChangePassword/>}></Route>
+        <Route path='/crearusers' element={<Crearusuarios/>}></Route>
+        <Route path='/ganadores' element={<Ganadores/>}></Route>
+        <Route path='/UserHome' element={<UserHome user={user}/>}></Route>
+        <Route path='/adminHome' element={<AdminHome user={user}/>}></Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+
+export default App
